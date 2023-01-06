0e1fbd7aeba6260de6878bfdf448747b3b6aae1f
andre <asozdanova@mail.ru>
2023-01-07 01:01:39 +0300

    Выделен класс для управлениями сессиями

--- test/test_untitled_test_case.py
+++ test/test_untitled_test_case.py
@@ -11,19 +11,19 @@ def app(request):
 
 def test_untitled_test_case(app):
         # login
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.init_group_creation()
     app.fill_group_firm(Group(name="test2", header="nhflflf", footer="nhfflf"))
     app.submit_group_creation()
-    app.logout()
+    app.session.logout()
 
 def test_untitled_test_case_empty(app):
         # login
-   app.login(username="admin", password="secret")
+   app.session.login(username="admin", password="secret")
    app.init_group_creation()
    app.fill_group_firm(Group(name="", header="", footer=""))
    app.submit_group_creation()
-   app.logout()
+   app.session.logout()
 
 
 
