8d02e86aebf1506cb05e365007fd07063d4c0a5e
andre <asozdanova@mail.ru>
2023-01-12 23:29:51 +0300

    Сравнение объектов

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,6 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
        self.wd = webdriver.Chrome(executable_path='C:\Python311\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe')
-       self.wd.implicitly_wait(5)
        self.session = SessionHelper(self)
        self.group = GroupHelper(self)
        self.contact = ContactHelper(self)
---
+++ geometry.py
@@ -0,0 +1,8 @@
+
+from geom2d.point import Point
+
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+#l2 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l2=list(l1)
+
+print(l1==l2)
