3b845b0abd20992dc6aba9f109ff314dd10442d8
andre <asozdanova@mail.ru>
2023-02-01 17:23:55 +0300

    Ещё один способ параметризации тестов: динамическая генерация тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 
 
-
+#@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, data_groups):
     group = data_groups
     old_groups = app.group.get_group_list()
