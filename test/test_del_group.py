e8740ec62e1cb1be751e499001fb300fcd07bdb1
andre <asozdanova@mail.ru>
2023-02-05 21:07:26 +0300

    Отключаемые проверки пользовательского интерфейса

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,7 @@
 from model.group import Group
 import random
 
-def test_delete_some_group(app,db):
+def test_delete_some_group(app,db, check_ui):
     if len(db.get_group_list())==0:#обращение к бд
         app.group.init_group_creation()
         app.group.fill_group_form(Group(name="test"))
@@ -14,3 +14,5 @@ def test_delete_some_group(app,db):
     assert len(old_groups) - 1 == len(new_groups) #проверка длины, метод count выступает в роли хеша
     old_groups.remove(group)#удаляется выбранный элемент
     assert old_groups == new_groups
+    if check_ui:#если высставлен флаг check_ui
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(),key=Group.id_or_max)# список из бд=список из ui
